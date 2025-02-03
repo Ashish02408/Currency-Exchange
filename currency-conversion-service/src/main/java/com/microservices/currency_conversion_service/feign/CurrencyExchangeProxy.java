2e7c2bb4c606c06cfc8299b4c64b63a001bd7fc9
Ashish02408 <54960573+Ashish02408@users.noreply.github.com>
2025-02-03 15:02:27 +0530

    Add files via upload

---
+++ currency-conversion-service/src/main/java/com/microservices/currency_conversion_service/feign/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package com.microservices.currency_conversion_service.feign;
+
+import com.microservices.currency_conversion_service.bean.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-conversion", url = "localhost:8000")
+//@FeignClient(name = "currency-conversion")
+public interface CurrencyExchangeProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
---
+++ currency-exchange-service/src/main/java/com/microservices/currency_exchange_service/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,10 @@
+package com.microservices.currency_exchange_service.repository;
+
+import com.microservices.currency_exchange_service.bean.CurrencyExchange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository
+        extends JpaRepository<CurrencyExchange,Long> {
+        CurrencyExchange findByFromAndTo(String From, String To);
+
+}
